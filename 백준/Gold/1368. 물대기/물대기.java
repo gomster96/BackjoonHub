35fcf7f3857644b0b95b9acad506d9f85e0fc40b
Byungwoong An <80206884+gomster96@users.noreply.github.com>
2023-01-05 15:08:37 +0900

    [Gold II] Title: 물대기, Time: 456 ms, Memory: 27148 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -0,0 +1,69 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Edge implements Comparable<Edge>{
+		int s, e, cost;
+		public Edge(int s, int e, int cost) {
+			this.s = s;
+			this.e = e;
+			this.cost = cost;
+		}
+		public int compareTo(Edge o) {
+			return cost - o.cost;
+		}
+	}
+	static int n;
+	static int[] w, parent, rank;
+	static int[][] p;
+	static PriorityQueue<Edge> pq = new PriorityQueue<>();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		w = new int[n+1];
+		parent = new int[n+1];
+		p = new int[n+1][n+1];
+		
+		for(int i=1; i<=n; i++) {
+			parent[i] = i;
+		}
+		for(int i=1; i<=n; i++) {
+			w[i] = Integer.parseInt(br.readLine());
+		}
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1; j<=n; j++) {
+				p[i][j] = Integer.parseInt(st.nextToken());
+				
+				// 여기서 가상의 노드 0을 만들고 0과 연결된 간선은 우물을 파는 비용으로 해야한다. 
+				if(i != j) pq.add(new Edge(i,j,p[i][j]));
+				else pq.add(new Edge(0,i,w[i]));
+			}
+		}
+		mst();
+	}
+	static void mst() {
+		int ret = 0;
+		while(!pq.isEmpty()) {
+			Edge edge = pq.poll();
+			int u = edge.s;
+			int v = edge.e;
+			if(find(u) == find(v)) continue;
+			union(u,v);
+			ret += edge.cost;		
+		}
+		System.out.println(ret);
+	}
+	static void union(int u, int v) {
+		u = find(u);
+		v = find(v);
+		parent[u] = v;
+	}
+	static int find(int x) {
+		if(x == parent[x]) return x;
+		return parent[x] = find(parent[x]);
+	}
+}
